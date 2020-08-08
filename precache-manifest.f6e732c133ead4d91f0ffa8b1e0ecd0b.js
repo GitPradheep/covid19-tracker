9702203a4ac10f81952838b3f2211a13a1ffc3d4
GitPradheep <67406204+GitPradheep@users.noreply.github.com>
2020-08-08 20:22:47 +0530

    Updates

---
+++ precache-manifest.f6e732c133ead4d91f0ffa8b1e0ecd0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a959e4de4c6658098b3c3313912d2c",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "c5c4ccbbc57f6dffb84d",
+    "url": "/covid19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "dd80af9180982e234a78",
+    "url": "/covid19-tracker/static/css/main.2419f58a.chunk.css"
+  },
+  {
+    "revision": "c5c4ccbbc57f6dffb84d",
+    "url": "/covid19-tracker/static/js/2.4f982a20.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid19-tracker/static/js/2.4f982a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd80af9180982e234a78",
+    "url": "/covid19-tracker/static/js/main.ae1cec2b.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  }
+]);
